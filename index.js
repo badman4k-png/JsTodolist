43e60ebf6c2f8a2fcf6d4db51ecd36f03584698b
Thanwarat Saengsai <69625706+badman4k-png@users.noreply.github.com>
2022-05-03 03:21:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -23,28 +23,3 @@ app.use('/api',require('./routers/route'))
 app.listen(3000, () => {
   console.log('server is running on Port 3000');
 })
-const express = require('express');
-const expressSession = require("express-session");
-const app = express();
-require('dotenv').config()
-
-const bodyparser = require('body-parser');
-const cors = require('cors');
-const db = require('./ConnectDB/datdabase');
-const route = require('./routers/route');
-
-app.use(bodyparser.json());
-app.use(bodyparser.urlencoded({ extended: true }));
-app.use(cors());
-app.use(route);
-app.use(expressSession({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: { }
-})); 
-app.use(express.static(__dirname + '/www'));
-app.use('/api',require('./routers/route'))
-app.listen(3000, () => {
-  console.log('server is running on Port 3000');
-})
