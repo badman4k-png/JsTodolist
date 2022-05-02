311dad1e597a07adcfbb93523d018ed117b9295d
Thanwarat Saengsai <69625706+badman4k-png@users.noreply.github.com>
2022-05-03 03:48:18 +0700

    Add files via upload

--- www/main.js
+++ www/main.js
@@ -1,59 +1,3 @@
-<<<<<<< HEAD
-var enterButton = document.getElementById("enter");
-var input = document.getElementById("userInput");
-var ul = document.querySelector("ul");
-var item = document.getElementsByTagName("li");
-
-function inputLength(){
-	return input.value.length;
-} 
-
-function listLength(){
-	return item.length;
-}
-
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = ""; 
-
-
-	function crossOut() {
-		li.classList.toggle("done");
-	}
-
-	li.addEventListener("click",crossOut);
-
-
-	var dBtn = document.createElement("button");
-	dBtn.appendChild(document.createTextNode("X"));
-	li.appendChild(dBtn);
-	dBtn.addEventListener("click", deleteListItem);
-
-
-	function deleteListItem(){
-		li.classList.add("delete")
-	}
-}
-
-
-function addListAfterClick(){
-	if (inputLength() > 0) {
-		createListElement();
-	}
-}
-
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.which ===13) {
-		createListElement();
-	} 
-}
-
-
-enterButton.addEventListener("click",addListAfterClick);
-
-=======
 var enterButton = document.getElementById("enter");
 var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
@@ -116,5 +60,4 @@ function addListAfterKeypress(event) {
 
 enterButton.addEventListener("click",addListAfterClick);
 
->>>>>>> 4d3acc7379925808a7fdf2c53b279b16e846718e
 input.addEventListener("keypress", addListAfterKeypress);
